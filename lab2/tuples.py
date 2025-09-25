e13581ff05152b0ec56334ec05067ace5241a8ae
Makisheva Tomiris <t_makisheva@kbtu.kz>
2025-09-25 12:49:24 +0500

    modified

--- lab2/tuples.py
+++ lab2/tuples.py
@@ -147,4 +147,6 @@ mytuple = fruits * 2
 
 print(mytuple)
 
-#
+"""python tuple methods
+count()	Returns the number of times a specified value occurs in a tuple
+index()	Searches the tuple for a specified value and returns the position of where it was found"""
