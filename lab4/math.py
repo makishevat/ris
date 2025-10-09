ae32815d0306715cee5ca2c0ddd2a0e7add8c96c
Makisheva Tomiris <t_makisheva@kbtu.kz>
2025-10-09 11:27:30 +0500

    new file

---
+++ lab4/math.py
@@ -0,0 +1,28 @@
+import math 
+#1
+"""degree=float(input("input degree:"))
+
+radian=degree*(math.pi/180)"""
+
+"""print("radian: ",round(radian,6))"""
+#2
+"""a=int(input())
+b=int(input())
+c=int(input())
+
+area=((a+b)/2)*c
+print (area)
+"""
+#3
+"""num=int(input("Numver of sides: "))
+len=int(input("Length of side: "))
+area=(num*(len**2))/(4*(math.tan(math.pi/num)))
+print(math.floor(area))"""
+
+#4
+
+base=int(input("Base:"))
+height=int(input("Height:"))
+area=base*height
+
+print(float(area))
