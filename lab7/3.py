455e04df7d6af5154cfa1493d56ffbfd1d20868b
Makisheva Tomiris <t_makisheva@kbtu.kz>
2025-10-30 15:07:37 +0500

    3 task

--- lab7/3.py
+++ lab7/3.py
@@ -3,4 +3,29 @@ import pygame
 pygame.init()
 screen=pygame.display.set_mode((800,800))
 clock=pygame.time.Clock()
-FPS
+FPS=50
+done=False
+posX=400
+posY=400
+
+while not done:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done=True
+        if event.type==pygame.KEYDOWN and event.key==pygame.K_UP:
+            if posY>20:
+                posY-=20
+        if event.type==pygame.KEYDOWN and event.key==pygame.K_DOWN:
+            if posY<760:
+                posY+=20
+        if event.type==pygame.KEYDOWN and event.key==pygame.K_LEFT:
+            if posX>20:
+                posX-=20
+        if event.type==pygame.KEYDOWN and event.key==pygame.K_RIGHT:
+            if posX<760:
+                posX+=20
+    screen.fill((255,255,255))
+    pygame.draw.circle(screen,(255,0,0),(posX,posY),25)
+    pygame.display.flip()
+    clock.tick(FPS)
+pygame.quit()
